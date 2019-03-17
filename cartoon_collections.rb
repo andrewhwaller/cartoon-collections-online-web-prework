f5739e846428eba9e660e53debb97527d5f422c2
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 06:03:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarf_names)
   dwarf_names.each_with_index { |dwarf_name, index| puts "#{index + 1}. #{dwarf_name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize << "!" }
 end
 
 def long_planeteer_calls# code an argument here
