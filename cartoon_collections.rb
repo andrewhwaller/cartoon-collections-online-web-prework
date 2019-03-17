58eb1ff564b7018538d1fedb3df4ce1943f93875
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 06:16:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(array)
-  array.detect?("cheddar" || "gouda" || "camembert")
+  array.detect("cheddar" || "gouda" || "camembert")
     return true
   # cheese_types = ["cheddar", "gouda", "camembert"]
 end
