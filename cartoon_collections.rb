61965733ae07c55ee6359bdcdf54b34811af2955
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 06:07:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  long_calls.map { |call| if call.length > 4 return true }
 end
 
 def find_the_cheese# code an argument here
